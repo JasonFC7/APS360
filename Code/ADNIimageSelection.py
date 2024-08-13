6e0ab15e69fb84300663f3ec9ec2174759bf060a
meredith <103847616+merlsey@users.noreply.github.com>
2024-08-13 16:18:49 -0400

    temp

--- Code/ADNIimageSelection.py
+++ Code/ADNIimageSelection.py
@@ -1,39 +1,39 @@
 import os
 import pydicom
 
-# def ImageSelect(data):
-#     root_dir = data
+def ImageSelect(data):
+    root_dir = data
 
-#     dicom_files = []
+    dicom_files = []
 
-#     for dirpath, dirnames, filenames in os.walk(root_dir):
-#         counter = 0
-#         breaker = 0
-#         if len(filenames) > 1:
-#             filenames = sorted(filenames, key=lambda x: int(x.split('_')[-3]))
-#             for file in filenames:
-#                 counter += 1
-#                 index = int(len(filenames) * 0.55)
-#                 if file.endswith('.dcm') and (counter == index) and (breaker == 0):
-#                     dicom_files.append(file)
-#                     breaker += 1
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        counter = 0
+        breaker = 0
+        if len(filenames) > 1:
+            filenames = sorted(filenames, key=lambda x: int(x.split('_')[-3]))
+            for file in filenames:
+                counter += 1
+                index = int(len(filenames) * 0.55)
+                if file.endswith('.dcm') and (counter == index) and (breaker == 0):
+                    dicom_files.append(file)
+                    breaker += 1
 
-#     return dicom_files
+    return dicom_files
         
 
-root_dir = 'ADNI 4'
+# root_dir = 'ADNI 4'
 
-dicom_files = []
+# dicom_files = []
 
-for dirpath, dirnames, filenames in os.walk(root_dir):
-    counter = 0
-    if len(filenames) > 1:
-        filenames = sorted(filenames, key=lambda x: int(x.split('_')[-3]))
-        for file in filenames:
-            counter += 1
-            index = len(filenames) / 3
-            if file.endswith('.dcm') and (counter == index):
-                dicom_files.append(os.path.join(dirpath, file))
+# for dirpath, dirnames, filenames in os.walk(root_dir):
+#     counter = 0
+#     if len(filenames) > 1:
+#         filenames = sorted(filenames, key=lambda x: int(x.split('_')[-3]))
+#         for file in filenames:
+#             counter += 1
+#             index = len(filenames) / 3
+#             if file.endswith('.dcm') and (counter == index):
+#                 dicom_files.append(os.path.join(dirpath, file))
 
-for filename in dicom_files:
-    print(filename)
+# for filename in dicom_files:
+#     print(filename)
